73018ab4ab78ea43128e659e22bda320b5fae216
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-03 19:10:20 +0530

    [Tushar Vidwans] extracting the information from Json and doing operation on the values

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace JsonInventoryManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadJson read = new ReadJson();
+
+            TypesOfInventary inventory = new TypesOfInventary();
+
+            inventory = read.ReadtheJson("C:/Users/tusha/source/repos/JsonInventoryManagement/JsonInventoryManagement/InventoryJson.json");
+
+            //Console.WriteLine(rice.price);
+            //Console.WriteLine(rice.weight);
+            //Console.WriteLine(rice.name);
+
+            int value = 0;
+
+            for (int i = 0; i < inventory.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(inventory.typesOfRice[i].name);
+                Console.WriteLine(inventory.typesOfRice[i].weight);
+                Console.WriteLine(inventory.typesOfRice[i].price);
+                value = inventory.typesOfRice[i].weight * inventory.typesOfRice[i].price;
+                Console.WriteLine($"Value of {inventory.typesOfRice[i].name} is {value}");
+            }
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("Types of Wheat: ");
+
+            for (int i = 0; i < inventory.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(inventory.typesOfWheat[i].name);
+                Console.WriteLine(inventory.typesOfWheat[i].weight);
+                Console.WriteLine(inventory.typesOfWheat[i].price);
+                value = inventory.typesOfWheat[i].weight * inventory.typesOfWheat[i].price;
+                Console.WriteLine($"Value of {inventory.typesOfWheat[i].name} is {value}");
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Types of Pulses: ");
+
+            for (int i = 0; i < inventory.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(inventory.typesOfPulses[i].name);
+                Console.WriteLine(inventory.typesOfPulses[i].weight);
+                Console.WriteLine(inventory.typesOfPulses[i].price);
+                value = inventory.typesOfPulses[i].weight * inventory.typesOfPulses[i].price;
+                Console.WriteLine($"Value of {inventory.typesOfPulses[i].name} is {value}");
+            }
+        }
+
+
+
+    }
+}
---
+++ ReadJson.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonInventoryManagement
+{
+    public class ReadJson
+    {
+
+        public TypesOfInventary ReadtheJson(string path)
+        {
+            using(StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string sfile = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<TypesOfInventary>(sfile);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+                
+            }
+        }
+
+    }
+}
